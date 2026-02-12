7ad719f5074adc7bd6d3cb85fabd8f168fd4fe40
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-02-12 16:27:48 +0530

    Create Day8x2.c

---
+++ 100Days-of-DSA/Day8x2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPowerOfTwo(int n) {
+    return (n > 0) && ((n & (n - 1)) == 0);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if (isPowerOfTwo(n))
+        printf("true");
+    else
+        printf("false");
+
+    return 0;
+}
