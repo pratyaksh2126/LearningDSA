495e3ee6b6ef9fd01862131e3f3e7b8ed148796c
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-02-17 20:15:31 +0530

    Create Day13x1.c

---
+++ 100Days-of-DSA/Day13x1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    // Read matrix dimensions
+    scanf("%d %d", &m, &n);
+
+    int A[m][n], B[m][n], Sum[m][n];
+
+    // Read first matrix
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Read second matrix
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Compute sum
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            Sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    // Print result matrix
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", Sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
