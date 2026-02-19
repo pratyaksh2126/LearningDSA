1758403fd77442ccd2c3c7527532a3ef15313e08
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-02-19 21:50:37 +0530

    Create Day15x1.c

---
+++ 100Days-of-DSA/Day15x1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    scanf("%d %d", &m, &n);
+
+    if (m != n) {
+        printf("Not a Symmetric Matrix");
+        return 0;
+    }
+
+    int matrix[m][n];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                printf("Not a Symmetric Matrix");
+                return 0;
+            }
+        }
+    }
+
+    printf("Symmetric Matrix");
+
+    return 0;
+}
