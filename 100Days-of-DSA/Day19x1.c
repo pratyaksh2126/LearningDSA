738b53a65bf09a126811715f471f883329e9edb0
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-02-23 15:29:16 +0530

    Create Day19x1.c

---
+++ 100Days-of-DSA/Day19x1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int diagonal_sum = 0;
+
+    for (int i = 0; i < m && i < n; i++) {
+        diagonal_sum += matrix[i][i];
+    }
+
+    printf("%d", diagonal_sum);
+
+    return 0;
+}
