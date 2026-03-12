d0e1e4e1054459a5ca131b1ec89029d0ccad17f2
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-03-12 18:54:03 +0530

    Create Day37x1.c

---
+++ 100Days-of-DSA/Day37x1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int value) {
+    if (top < MAX - 1) {
+        top++;
+        stack[top] = value;
+    }
+}
+
+void pop() {
+    if (top >= 0) {
+        top--;
+    }
+}
+
+int main() {
+    int n, m, i, x;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &x);
+        push(x);
+    }
+
+    scanf("%d", &m);
+
+    for (i = 0; i < m; i++) {
+        pop();
+    }
+
+    for (i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+
+    return 0;
+}
