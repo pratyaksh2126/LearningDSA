de3a2f795a12100711626da4a186ae9c72876298
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-03-18 12:33:42 +0530

    Create Day43x1.c

---
+++ 100Days-of-DSA/Day43x1.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#define MAX 100
+
+int deque[MAX];
+int front = -1, rear = -1;
+
+void push_front(int x){
+    if((front == 0 && rear == MAX-1) || (front == rear+1)) return;
+    if(front == -1){
+        front = rear = 0;
+    } else if(front == 0){
+        front = MAX-1;
+    } else {
+        front--;
+    }
+    deque[front] = x;
+}
+
+void push_back(int x){
+    if((front == 0 && rear == MAX-1) || (front == rear+1)) return;
+    if(front == -1){
+        front = rear = 0;
+    } else if(rear == MAX-1){
+        rear = 0;
+    } else {
+        rear++;
+    }
+    deque[rear] = x;
+}
+
+void pop_front(){
+    if(front == -1) return;
+    if(front == rear){
+        front = rear = -1;
+    } else if(front == MAX-1){
+        front = 0;
+    } else {
+        front++;
+    }
+}
+
+void pop_back(){
+    if(front == -1) return;
+    if(front == rear){
+        front = rear = -1;
+    } else if(rear == 0){
+        rear = MAX-1;
+    } else {
+        rear--;
+    }
+}
+
+void display(){
+    if(front == -1) return;
+    int i = front;
+    while(1){
+        printf("%d ", deque[i]);
+        if(i == rear) break;
+        i = (i + 1) % MAX;
+    }
+    printf("\n");
+}
+
+int main(){
+    push_back(10);
+    push_back(20);
+    push_front(5);
+    push_front(2);
+    pop_back();
+    pop_front();
+    display();
+    return 0;
+}
