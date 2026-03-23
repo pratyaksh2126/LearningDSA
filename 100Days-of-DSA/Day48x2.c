9142d6d2d5fc78ac4f503ca3eded9708507c5554
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-03-23 22:18:27 +0530

    Create Day48x2.c

---
+++ 100Days-of-DSA/Day48x2.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct TreeNode* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct TreeNode** queue = (struct TreeNode**)malloc(n * sizeof(struct TreeNode*));
+    int front = 0, rear = 0;
+
+    struct TreeNode* root = newNode(arr[0]);
+    queue[rear++] = root;
+
+    int i = 1;
+    while (i < n) {
+        struct TreeNode* curr = queue[front++];
+
+        if (arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    free(queue);
+    return root;
+}
+
+void inorder(struct TreeNode* root) {
+    struct TreeNode* stack[100];
+    int top = -1;
+    struct TreeNode* curr = root;
+
+    while (curr != NULL || top != -1) {
+        while (curr != NULL) {
+            stack[++top] = curr;
+            curr = curr->left;
+        }
+
+        curr = stack[top--];
+        printf("%d ", curr->val);
+        curr = curr->right;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int* arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct TreeNode* root = buildTree(arr, n);
+    inorder(root);
+
+    free(arr);
+    return 0;
+}
