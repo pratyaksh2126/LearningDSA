3933dede30aa24027d1cefe3a802b4486b1e7884
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-03-25 23:01:30 +0530

    Create Day50x2.c

---
+++ 100Days-of-DSA/Day50x2.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct TreeNode* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct TreeNode** queue = (struct TreeNode**)malloc(n * sizeof(struct TreeNode*));
+    int front = 0, rear = 0;
+
+    struct TreeNode* root = newNode(arr[0]);
+    queue[rear++] = root;
+
+    int i = 1;
+
+    while (i < n) {
+        struct TreeNode* curr = queue[front++];
+
+        if (i < n && arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    free(queue);
+    return root;
+}
+
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    return (left > right ? left : right) + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int* arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct TreeNode* root = buildTree(arr, n);
+    printf("%d", maxDepth(root));
+
+    free(arr);
+    return 0;
+}
