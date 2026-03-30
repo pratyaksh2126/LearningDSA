947c1a9ebed02086367f840080e77b476ef43723
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-03-30 21:49:18 +0530

    Create Day54x2.c

---
+++ 100Days-of-DSA/Day54x2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
+    if (root == NULL)
+        return createNode(val);
+
+    if (val < root->val)
+        root->left = insertIntoBST(root->left, val);
+    else
+        root->right = insertIntoBST(root->right, val);
+
+    return root;
+}
+
+void inorder(struct TreeNode* root) {
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->val);
+    inorder(root->right);
+}
+
+int main() {
+    struct TreeNode* root = NULL;
+    int n, x, val;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insertIntoBST(root, x);
+    }
+
+    scanf("%d", &val);
+
+    root = insertIntoBST(root, val);
+
+    inorder(root);
+
+    return 0;
+}
