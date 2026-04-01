d11cf5e3cef1a4e201c04de2b2262dc4dccc4232
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-04-01 16:04:19 +0530

    Create Day56x1.c

---
+++ 100Days-of-DSA/Day56x1.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL)
+        return newNode(data);
+
+    if (data < root->data)
+        root->left = insert(root->left, data);
+    else
+        root->right = insert(root->right, data);
+
+    return root;
+}
+
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    if (root == NULL)
+        return NULL;
+
+    if (n1 < root->data && n2 < root->data)
+        return findLCA(root->left, n1, n2);
+
+    if (n1 > root->data && n2 > root->data)
+        return findLCA(root->right, n1, n2);
+
+    return root;
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    struct Node* root = NULL;
+
+    for (int i = 0; i < N; i++) {
+        int val;
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* lca = findLCA(root, n1, n2);
+
+    if (lca != NULL)
+        printf("%d\n", lca->data);
+
+    return 0;
+}
