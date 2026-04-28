d642008c2b5655756ce4dd7a24595355de4d543e
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-04-28 23:11:35 +0530

    Create Day90x2.c

---
+++ 100Days-of-DSA/Day90x2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int findMin(int* nums, int numsSize) {
+    int left = 0, right = numsSize - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] > nums[right])
+            left = mid + 1;
+        else
+            right = mid;
+    }
+
+    return nums[left];
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int nums[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &nums[i]);
+
+    int result = findMin(nums, n);
+    printf("%d", result);
+
+    return 0;
+}
