7e4c797a03816e7c5aeb0d8d200f8c06b3e50758
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-05-18 20:53:31 +0530

    Create Day113x1.c

---
+++ 100Days-of-DSA/Day113x1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+void push(int value) {
+    if(top == MAX - 1) {
+        printf("Stack Overflow\n");
+    } else {
+        top++;
+        stack[top] = value;
+        printf("%d pushed into stack\n", value);
+    }
+}
+
+void pop() {
+    if(top == -1) {
+        printf("Stack Underflow\n");
+    } else {
+        printf("%d popped from stack\n", stack[top]);
+        top--;
+    }
+}
+
+void display() {
+    int i;
+
+    if(top == -1) {
+        printf("Stack is Empty\n");
+    } else {
+        printf("Stack Elements: ");
+        for(i = top; i >= 0; i--) {
+            printf("%d ", stack[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+
+    display();
+
+    pop();
+
+    display();
+
+    return 0;
+}
