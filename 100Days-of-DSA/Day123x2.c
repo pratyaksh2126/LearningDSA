e7a9bf13e7f78a17e4b2de8c73a6fb53117be78a
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-05-31 23:54:32 +0530

    Create Day123x2.c

---
+++ 100Days-of-DSA/Day123x2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define MAX 5
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+void enqueue(int x) {
+    if(rear == MAX - 1)
+        printf("Queue Overflow\n");
+    else {
+        if(front == -1)
+            front = 0;
+        queue[++rear] = x;
+    }
+}
+
+void dequeue() {
+    if(front == -1 || front > rear)
+        printf("Queue Underflow\n");
+    else
+        printf("Deleted: %d\n", queue[front++]);
+}
+
+int main() {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    dequeue();
+    dequeue();
+
+    return 0;
+}
