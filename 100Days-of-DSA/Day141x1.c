2ced2133da6ca5b25b2a2599aca81942727fb084
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-06-27 01:04:23 +0530

    Create Day141x1.c

---
+++ 100Days-of-DSA/Day141x1.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+#define SIZE 5
+
+int queue[SIZE];
+int front = -1, rear = -1;
+
+void enqueue(int value) {
+    if ((rear + 1) % SIZE == front) {
+        printf("Queue Overflow\n");
+        return;
+    }
+
+    if (front == -1)
+        front = 0;
+
+    rear = (rear + 1) % SIZE;
+    queue[rear] = value;
+}
+
+void dequeue() {
+    if (front == -1) {
+        printf("Queue Underflow\n");
+        return;
+    }
+
+    printf("Deleted: %d\n", queue[front]);
+
+    if (front == rear)
+        front = rear = -1;
+    else
+        front = (front + 1) % SIZE;
+}
+
+void display() {
+    if (front == -1) {
+        printf("Queue Empty\n");
+        return;
+    }
+
+    int i = front;
+
+    printf("Queue: ");
+
+    while (1) {
+        printf("%d ", queue[i]);
+
+        if (i == rear)
+            break;
+
+        i = (i + 1) % SIZE;
+    }
+
+    printf("\n");
+}
+
+int main() {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    display();
+
+    dequeue();
+
+    display();
+
+    enqueue(40);
+    enqueue(50);
+    enqueue(60);
+
+    display();
+
+    return 0;
+}
