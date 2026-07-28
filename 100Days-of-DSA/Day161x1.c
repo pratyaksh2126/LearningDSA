ae9f19ba01e63de68f03593539a68c769f4457a6
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-07-28 22:36:12 +0530

    Create Day161x1.c

---
+++ 100Days-of-DSA/Day161x1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+#define MAX 5
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+void enqueue(int value) {
+    if (rear == MAX - 1) {
+        printf("Queue Overflow\n");
+        return;
+    }
+
+    if (front == -1)
+        front = 0;
+
+    queue[++rear] = value;
+}
+
+void dequeue() {
+    if (front == -1 || front > rear) {
+        printf("Queue Underflow\n");
+        return;
+    }
+
+    printf("Deleted Element: %d\n", queue[front++]);
+
+    if (front > rear)
+        front = rear = -1;
+}
+
+void display() {
+    int i;
+
+    if (front == -1) {
+        printf("Queue is Empty\n");
+        return;
+    }
+
+    printf("Queue: ");
+
+    for (i = front; i <= rear; i++)
+        printf("%d ", queue[i]);
+
+    printf("\n");
+}
+
+int main() {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    display();
+
+    dequeue();
+
+    display();
+
+    return 0;
+}
