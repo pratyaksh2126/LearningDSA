11acef41eb7059fbf1537d0c62132b2235f4a29c
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-08-06 16:53:41 +0530

    Create Day167x1.c

---
+++ 100Days-of-DSA/Day167x1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+struct Node *top = NULL;
+
+void push(int value) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+}
+
+void pop() {
+    if (top == NULL) {
+        printf("Stack Underflow\n");
+        return;
+    }
+
+    struct Node *temp = top;
+    printf("Popped: %d\n", top->data);
+    top = top->next;
+    free(temp);
+}
+
+void display() {
+    struct Node *temp = top;
+
+    if (temp == NULL) {
+        printf("Stack is Empty\n");
+        return;
+    }
+
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+
+    display();
+
+    pop();
+
+    display();
+
+    return 0;
+}
