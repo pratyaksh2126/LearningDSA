5267528b983650384267edb9946168c83e92d424
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-08-16 23:23:24 +0530

    Create Day177x2.c

---
+++ 100Days-of-DSA/Day177x2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key) return mid;
+        else if (arr[mid] < key) low = mid + 1;
+        else high = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9};
+    int n = 5, key = 7;
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1) printf("Found at index %d", result);
+    else printf("Not Found");
+
+    return 0;
+}
