e4d9eeb2815017dfcb8cad7b0cb45b1df2fa11d6
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-09-19 22:58:08 +0530

    Create Day211x2.c

---
+++ 100Days-of-DSA/Day211x2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, key, pos = -1;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            pos = i;
+            break;
+        }
+    }
+
+    if(pos != -1)
+        printf("Found at index %d", pos);
+    else
+        printf("Not found");
+
+    return 0;
+}
